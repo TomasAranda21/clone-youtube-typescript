5ec3697cb03d807252ecab962d7af15ed7a27c4d
Tomas Aranda <tomasaranda2018@gmail.com>
2023-02-28 20:46:43 -0300

    add api movies

---
+++ src/utils/apiMovies.tsx
@@ -0,0 +1,61 @@
+export const api = [
+    {
+        name: 'Billie Eilish, Cigarettes After Sex, Carla Morrison, Zubi, Emma Peters',
+        img: 'https://res.cloudinary.com/dj1pp4ivb/image/upload/v1658463689/hqdefault_4_w4mx9q.webp',
+        time: '3:20:02',
+        views: '4.700',
+        icon: 'https://res.cloudinary.com/dj1pp4ivb/image/upload/v1658463698/channels4_profile_5_ga0luc.jpg',
+        nameChannel: 'Universe Nation',
+        date: '11 hours',
+    }
+]
+
+export const apiBusquedas = [
+
+    'All',
+    'Music',
+    'Live',
+    'Gaming',
+    'Computer Keyboard',
+    'Programming',
+    'JavaScript',
+    'Podcasts',
+    'Deep House',
+    'DJ mix',
+    'Instrumentals',
+    'Classical Music',
+    'Soft Rock',
+    'Jazz',
+    'Piano',
+    'History',
+    'Breaking bad',
+    'Recently uploaded',
+    'Watched',
+    'New to you',
+  
+]
+
+
+export const subscriptions = [
+    {
+        img: 'https://res.cloudinary.com/dj1pp4ivb/image/upload/v1658550725/subs/unnamed_10_xkylvx.jpg',
+        name: 'Oscar Barajas'
+    },
+    {
+        img: 'https://res.cloudinary.com/dj1pp4ivb/image/upload/v1658550725/subs/unnamed_yazrj3.jpg',
+        name: 'Learn English with ...'
+    },
+    {
+        img: 'https://res.cloudinary.com/dj1pp4ivb/image/upload/v1658550725/subs/channels4_profile_nhjyif.jpg',
+        name: 'Tomorrowland'
+    },
+    {
+        img: 'https://res.cloudinary.com/dj1pp4ivb/image/upload/v1658550725/subs/unnamed_2_ov2s1s.jpg',
+        name: 'midudev'
+    },
+    {
+        img: 'https://res.cloudinary.com/dj1pp4ivb/image/upload/v1658550725/subs/unnamed_1_b6wnnd.jpg',
+        name: 'jonmircha'
+    }
+   
+] 
