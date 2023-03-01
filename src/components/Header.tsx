5557acf1d387c2a68899c7a214d375d257d8ffd4
Tomas Aranda <tomasaranda2018@gmail.com>
2023-02-28 21:25:50 -0300

    change styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = ({searchCarousel} :props  )  => {
   return (
 
     <div className={'w-full flex flex-col sticky top-0 z-50'}>
-        <div className={'w-full py-3 px-5 flex justify-between mx-auto items-center relative'}>
+        <div className={'w-full py-3 px-6 flex justify-between mx-auto items-center relative'}>
 
         <div className={'relative flex items-center gap-4 text-white text-xl'}>
             {icons.navIcon}
@@ -22,13 +22,13 @@ const Header = ({searchCarousel} :props  )  => {
 
         </div>
 
-          <div className={'flex items-center gap-4 w-1/3'}>
-            <div className={'flex items-center rounded shadow-lg bg-white/10'}>
-              <input type="text" className={'text-center px-20 text-white'} placeholder="Search"/>
-              <p className={'cursor-pointer'}>{icons.searchIcon}</p>
+          <div className={'flex items-center gap-3 w-[80%] justify-center mx-auto '}>
+            <div className={'flex items-center rounded-2xl w-[45%] shadow-lg bg-white/10 border border-white/30'}>
+              <input type="text" className={'rounded-2xl w-full px-4 py-1.5 border-r border-white/30 rounded-r-none text-white bg-[#121212] outline-0'} placeholder="Search"/>
+              <p className={'cursor-pointer w-14 mx-auto flex justify-center text-white text-[18px] text-center'}>{icons.searchIcon}</p>
             </div>
 
-              <p className={'text-center px-20 text-white'}>{icons.micIcon}</p>
+              <p className={'text-center text-white bg-gray-400/10 p-3 rounded-full'}>{icons.micIcon}</p>
           </div>
       </div>
 
