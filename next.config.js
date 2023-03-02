874fbb16d9fbd1086a077c55f7a8239977f594d6
Tomas Aranda <tomasaranda2018@gmail.com>
2023-03-02 01:20:55 -0300

    add image url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com', 'i.ytimg.com', 'yt3.ggpht.com'],
   },
 }
 
