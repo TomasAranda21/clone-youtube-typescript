d2799ad1d34c4da6b73cb499f3d027acb6451b97
Tomas Aranda <tomasaranda2018@gmail.com>
2023-03-02 01:22:06 -0300

    change styles

--- src/components/LiAtributte.tsx
+++ src/components/LiAtributte.tsx
@@ -14,8 +14,8 @@ interface LiSubsProps {
 export const Li = ({text, icon, type}: LiProps) => {
   return (
     <div className={''}>
-        <li className={type === 'text' ? 'text-white/40' : 'flex items-center text-white gap-2 font-medium px-3 py-2 my-3 cursor-pointer text-sm hover:bg-[#f1f1f1]/10 rounded-lg mx-3'}>
-            <p className='text-lg'>{icon}</p>
+        <li className={type === 'text' ? 'text-white/40' : 'flex items-center font-light text-white gap-2 px-3 py-[10px]  cursor-pointer text-sm hover:bg-[#f1f1f1]/10 rounded-lg mx-1'}>
+            <p className='text-[18px]'>{icon}</p>
             <p className='text-white ml-4 capitalize'>{text?.toLowerCase()}</p>
         </li>
     </div>
@@ -27,11 +27,11 @@ export const Li = ({text, icon, type}: LiProps) => {
 export const LiSubs = ({text, img} : LiSubsProps) => {
   return (
     <div className={''}>
-        <li className={'flex items-center gap-4 font-medium px-3 py-3 mx-3'}>
+        <li className={'flex items-center gap-4 font-medium px-3 py-3 mx-1'}>
             <div className={'w-[24px] h-[24px]'}>
                 <Image src={img} width={24} height={24} alt="image" className='absolute rounded-full object-cover' />
             </div>
-            <p className='text-center text-gray-50'>{text}</p>
+            <p className='text-center text-sm font-light text-gray-50'>{text}</p>
         </li>
     </div>
   )
