5247408e3ca4f47a40a74e269afe8094c1d1c5af
Tomas Aranda <tomasaranda2018@gmail.com>
2023-04-10 13:35:50 -0300

    remove context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
-import { ApiProvider } from '@/context/ApiContext'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApiProvider>
     <Component {...pageProps} />
-  </ApiProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Homelayout from '@/layout/HomeLayout'
 import Wrapper from '@/components/Wrapper'
-import Header from '@/components/Header'
-import CardsMovies from '@/components/CardsMovies'
+import CardsVideos from '@/components/CardsVideos'
 import { useEffect, useState } from 'react'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [data, setData] = useState<any>([])
 
   useEffect(() => {
-    const doFetch = async () => {
-      // const url = 'https://www.googleapis.com/youtube/v3/playlistItems'
-      
-      const options = {
-        part: 'snippet,contentDetails',
-        key: process.env.NEXT_PUBLIC_APIKEY,
-        playListId: `PLeGhvhmT-FQT4kZ7HQQdvryL_imDvlljm`,
-        maxResult: 20
+      const doFetch = async () => {
+        const options = {
+          part: 'snippet,contentDetails',
+          key: process.env.NEXT_PUBLIC_APIKEY,
+          playListId: `PLeGhvhmT-FQT4kZ7HQQdvryL_imDvlljm`,
+          maxResult: 20
+        }
+        
+        let url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=${options.part}&maxResults=${options.maxResult}&playlistId=${options.playListId}&key=${options.key}`
+  
+        const res = await fetch(url)
+        const json = await res.json()
+        // console.log(json)
+        setData(json.items)
       }
-      
-      let url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=${options.part}&maxResults=${options.maxResult}&playlistId=${options.playListId}&key=${options.key}`
-
-      const res = await fetch(url)
-      const json = await res.json()
-      setData(json.items)
-   
-    }
-
-    doFetch()
-  }, [])
+      doFetch()
+    }, [])
 
   return (
     <Homelayout>
-      <Header/>
       <Wrapper>
-        {data.map((data : any) => {
+        {data?.map((data : any) => {
           return (
-            <div key={data.id}>
-            <CardsMovies videoId={data.contentDetails.videoId}/>
+            <div key={`${data.id}12`}>
+            <CardsVideos videoId={data.contentDetails.videoId}/>
           </div>
           )
         })}
